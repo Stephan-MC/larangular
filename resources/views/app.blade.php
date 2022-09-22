42c3fb817b3ff9ab49be84f2703a8b0c2e8ea9fb
Stephan Munde <mundestephane13@gmail.com>
2022-09-22 21:03:05 +0100

    Initial Commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Angular</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="styles.ef46db3751d8e999.css">
+</head>
+
+<body>
+    <app-root></app-root>
+    <script src="{{ asset('build/ng/runtime.27845ceb5aedcb1f.js') }} " type="module"></script>
+    <script src="{{ asset('build/ng/polyfills.03d72e078c527090.js') }}" type="module"></script>
+    <script src="{{ asset('build/ng/main.1d3568d459dd85d0.js') }}" type="module"></script>
+
+</body>
+
+</html>
